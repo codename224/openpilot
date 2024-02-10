e561de3eea85ab9604ecd3eec695434628d59549
codename224 <4961059+codename224@users.noreply.github.com>
2024-02-09 20:00:13 -0800

    Change startup to ready to drive

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -184,7 +184,7 @@ class NormalPermanentAlert(Alert):
 
 
 class StartupAlert(Alert):
-  def __init__(self, alert_text_1: str, alert_text_2: str = "Always keep hands on wheel and eyes on road", alert_status=AlertStatus.normal):
+  def __init__(self, alert_text_1: str, alert_text_2: str = "Ready to Drive", alert_status=AlertStatus.normal):
     super().__init__(alert_text_1, alert_text_2,
                      alert_status, AlertSize.mid,
                      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 5.),
